da8f6882b8f6fa8bdc2356a1eb453ae5a8feec45
Amila Welihinda <amilajack@gmail.com>
2022-09-23 17:52:42 -0700

    misc

--- src/index.js
+++ src/index.js
@@ -22,7 +22,6 @@ import { Root } from './components/app/Root';
 import createStore from './app-logic/create-store';
 import {
   addDataToWindowObject,
-  logFriendlyPreamble,
   logDevelopmentTips,
 } from './utils/window-console';
 import { ensureExists } from './utils/flow';
@@ -56,10 +55,6 @@ render(
 );
 
 addDataToWindowObject(store.getState, store.dispatch);
-if (process.env.NODE_ENV === 'production') {
-  // Don't clutter the console in development mode.
-  logFriendlyPreamble();
-}
 if (process.env.NODE_ENV === 'development') {
   logDevelopmentTips();
 }
